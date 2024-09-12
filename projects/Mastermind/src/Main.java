725533785c52924a078972dfdc3a24332817b0fa
MisterNo0ne <84687450+MisterNo0ne@users.noreply.github.com>
2024-09-12 14:11:13 -0600

    they call me mr awake

---
+++ projects/Mastermind/src/Main.java
@@ -0,0 +1,36 @@
+//import java.util.Scanner;
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		
+		
+		try {
+			File textFile = new File("input.txt");
+			String randomWord;
+			Scanner myReader = new Scanner(textFile);
+			while (myReader.hasNextLine()) {
+	        	String data = myReader.nextLine();
+	        	System.out.println(data);
+	      	}
+		} catch (FileNotFoundException e) {
+			System.out.println("UGHH");
+		}
+		
+		
+		
+		
+		
+//		System.out.println("Hello world!");
+		
+		
+		
+		/*
+		File myObj = new File("filename.txt");
+      	Scanner myReader = new Scanner(myObj);
+      	
+      	myReader.close();   */
+	}
+}
